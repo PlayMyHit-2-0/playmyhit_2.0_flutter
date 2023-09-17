a702cd2b6ea139516572d25ba3c3cb2c7eaf9f62
Aldanis Vigo <aldanisvigo@gmail.com>
2023-09-17 11:31:43 -0500

    added logic to add new posts for the currently logged in user in the posts repository fixes #9

---
+++ lib/data/repositories/posts_repo.dart
@@ -0,0 +1,81 @@
+import 'dart:async';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/foundation.dart';
+import 'package:playmyhit/data/models/post.dart';
+
+class PostsRepository {
+  FirebaseFirestore firestore;
+  FirebaseStorage storage;
+  FirebaseAuth auth;
+  PostsRepository({required this.firestore, required this.storage, required this.auth});
+  StreamSubscription? currentPostsListener;
+  List<Post>? posts;
+
+  // When the posts repository is disposed
+  void dispose(){
+    // Cancel the current posts subscription
+    currentPostsListener?.cancel();
+  }
+
+  //Write logic to get posts
+  Future<bool> listenToPostsForUid(String uid) async {
+    try{  
+      
+      // Get a reference to the posts collection for the given UID
+      CollectionReference ref = firestore.collection("/users/$uid/posts");
+
+      // Start listening to the posts collection 
+      currentPostsListener = ref.snapshots().listen((event) { 
+        posts = List.empty(); // Empty the list of current posts
+        // Fill it with the new posts
+        for (var element in event.docs) {
+            posts?.add(Post.fromJson(element.data() as Map<String,dynamic>));
+        }
+
+        if(kDebugMode){
+          print(posts);
+        }
+      });
+
+      if(kDebugMode){
+        print("Listening to posts collection for user with uid: $uid");
+      }
+
+      return Future.value(true);
+    }catch(e){
+      if(kDebugMode){
+        print(e.toString());
+      }
+      rethrow;
+    }
+  }
+
+  // Write logic to create posts
+  Future<bool> createPost(Post post) async {
+    try{
+      if(auth.currentUser?.uid != null){
+        CollectionReference ref = firestore.collection("users/${auth.currentUser?.uid}/posts");
+        await ref.add(post.toJson());
+        return true;
+      }else{
+        throw Exception("You need to login to make a new post.");
+      }
+    }catch(e){
+      if(kDebugMode){
+        print(e.toString());
+      }
+      rethrow;
+    }
+  }
+
+
+  // TODO Write logic to update posts
+  // TODO Write logic to delete posts
+  // TODO Write logic to like posts
+  // TODO Write logic to comment on a post
+  // TODO Write logic to edit a comment on a post
+  // TODO Write logic to delete a post comment
+}
